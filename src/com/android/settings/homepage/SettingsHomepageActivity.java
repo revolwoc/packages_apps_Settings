a18fe91b727571e314e9901defdd5a4751f1e540
Nauval Rizky <enuma.alrizky@gmail.com>
2021-08-30 12:11:45 +0700

    Settings: Improve A12-styled Searchbar layout
    - Add top padding on homepage conditionally to fix overlapped cards
    - Remove unnecessary backgrounds on main content and container
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -40,6 +40,8 @@ import com.android.settings.core.HideNonSystemOverlayMixin;
 import com.android.settings.homepage.contextualcards.ContextualCardsFragment;
 import com.android.settings.overlay.FeatureFactory;
 
+import com.google.android.material.appbar.AppBarLayout;
+
 public class SettingsHomepageActivity extends FragmentActivity {
 
     @Override
@@ -57,7 +59,10 @@ public class SettingsHomepageActivity extends FragmentActivity {
         root.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
-        setHomepageContainerPaddingTop();
+        if (useNewSearchBar)
+            setHomepageContainerPaddingTopA12();
+        else
+            setHomepageContainerPaddingTop();
 
         final Toolbar toolbar = findViewById(R.id.search_action_bar);
         FeatureFactory.getFactory(this).getSearchFeatureProvider()
@@ -95,6 +100,20 @@ public class SettingsHomepageActivity extends FragmentActivity {
         // The top padding is the height of action bar(48dp) + top/bottom margins(16dp)
         final int paddingTop = searchBarHeight + searchBarMargin * 2;
         view.setPadding(0 /* left */, paddingTop, 0 /* right */, 0 /* bottom */);
+
+        // Prevent inner RecyclerView gets focus and invokes scrolling.
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+    }
+
+    private void setHomepageContainerPaddingTopA12() {
+        final View view = findViewById(R.id.homepage_container);
+        final AppBarLayout appBarLayout = findViewById(R.id.appbar_layout_a12);
+
+        // use appBarLayout's current height
+        appBarLayout.post(() -> {
+            view.setPadding(0, appBarLayout.getMeasuredHeight(), 0, 0);
+        });
 
         // Prevent inner RecyclerView gets focus and invokes scrolling.
         view.setFocusableInTouchMode(true);

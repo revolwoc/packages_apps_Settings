504f6dde6a0e25f818b3cff90601e6de7429d89a
LuK1337 <priv.luk@gmail.com>
2021-09-22 03:38:25 +0900

    Settings: Refactor firmware version window
    * Add our Easter Egg into our ROM version preference
    * Follow LineageOS' format for Evolution X version: https://github.com/LineageOS/android_packages_apps_Settings/commit/224fe65e326c372ed2bf4241ab5975786888afb2
    * Remove redundant device codename preference
    
    Revert "Settings: Add Device codename to Firmware version window"
    
    This reverts commit 20f7652424910b690297a9c5c9f5e4490d1c537c.
    
    Co-authored-by: Stallix <joey@evolution-x.org>

--- src/com/android/settings/deviceinfo/firmwareversion/EvolutionXVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/EvolutionXVersionPreferenceController.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2019 The LineageOS Project
  * Copyright (C) 2019-2021 The Evolution X Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,31 +19,46 @@ package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.os.Build;
+import android.os.SystemClock;
 import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.UserManager;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
 import com.android.settings.R;
+import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.slices.Sliceable;
+import com.android.settingslib.RestrictedLockUtils;
+import com.android.settingslib.RestrictedLockUtilsInternal;
 
 public class EvolutionXVersionPreferenceController extends BasePreferenceController {
 
-    private static final Uri INTENT_URI_DATA = Uri.parse("https://paypal.me/joeyhuab/");
-    private static final String TAG = "EvolutionXVersionPreferenceCtrl";
-    private static final String KEY_ROM_VERSION_PROP = "org.evolution.build_version";
-    private static final String KEY_ROM_CODENAME_PROP = "org.evolution.build_codename";
-    private static final String KEY_ROM_BUILD_TYPE_PROP = "org.evolution.build_type";
+    private static final String TAG = "evolutionXVersionDialogCtrl";
+    private static final int DELAY_TIMER_MILLIS = 500;
+    private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
-    private final PackageManager mPackageManager;
+    private static final String KEY_EVOLUTION_VERSION_PROP = "org.evolution.version.display";
+
+    private static final String PLATLOGO_PACKAGE_NAME = "org.evolution.easteregg";
+    private static final String PLATLOGO_ACTIVITY_CLASS =
+            PLATLOGO_PACKAGE_NAME + ".EasterEgg";
+
+    private final UserManager mUserManager;
+    private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
+
+    private RestrictedLockUtils.EnforcedAdmin mFunDisallowedAdmin;
+    private boolean mFunDisallowedBySystem;
 
     public EvolutionXVersionPreferenceController(Context context, String key) {
         super(context, key);
-        mPackageManager = mContext.getPackageManager();
+        mUserManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
+        initializeAdminPermissions();
     }
 
     @Override
@@ -51,14 +67,19 @@ public class EvolutionXVersionPreferenceController extends BasePreferenceControl
     }
 
     @Override
+    public boolean useDynamicSliceSummary() {
+        return true;
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
     public CharSequence getSummary() {
-        String romVersion = SystemProperties.get(KEY_ROM_VERSION_PROP,
-                mContext.getString(R.string.device_info_default));
-        String romCodename = SystemProperties.get(KEY_ROM_CODENAME_PROP,
-                mContext.getString(R.string.device_info_default));
-        String romBuildtype = SystemProperties.get(KEY_ROM_BUILD_TYPE_PROP,
-                mContext.getString(R.string.device_info_default));
-        return romVersion + " " + romCodename + " | " + romBuildtype;
+        return SystemProperties.get(KEY_EVOLUTION_VERSION_PROP,
+                mContext.getString(R.string.unknown));
     }
 
     @Override
@@ -66,17 +87,51 @@ public class EvolutionXVersionPreferenceController extends BasePreferenceControl
         if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
             return false;
         }
-
-        final Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(INTENT_URI_DATA);
-        if (mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
-            // Don't send out the intent to stop crash
-            Log.w(TAG, "queryIntentActivities() returns empty");
-            return true;
+        if (Utils.isMonkeyRunning()) {
+            return false;
         }
+        arrayCopy();
+        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+        if (mHits[0] >= (SystemClock.uptimeMillis() - DELAY_TIMER_MILLIS)) {
+            if (mUserManager.hasUserRestriction(UserManager.DISALLOW_FUN)) {
+                if (mFunDisallowedAdmin != null && !mFunDisallowedBySystem) {
+                    RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mContext,
+                            mFunDisallowedAdmin);
+                }
+                Log.d(TAG, "Sorry, no fun for you!");
+                return true;
+            }
 
-        mContext.startActivity(intent);
+            final Intent intent = new Intent(Intent.ACTION_MAIN)
+                     .setClassName(PLATLOGO_PACKAGE_NAME, PLATLOGO_ACTIVITY_CLASS);
+            try {
+                mContext.startActivity(intent);
+            } catch (Exception e) {
+                Log.e(TAG, "Unable to start activity " + intent.toString());
+            }
+        }
         return true;
+    }
+
+    /**
+     * Copies the array onto itself to remove the oldest hit.
+     */
+    @VisibleForTesting
+    void arrayCopy() {
+        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
+    }
+
+    @VisibleForTesting
+    void initializeAdminPermissions() {
+        mFunDisallowedAdmin = RestrictedLockUtilsInternal.checkIfRestrictionEnforced(
+                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
+        mFunDisallowedBySystem = RestrictedLockUtilsInternal.hasBaseUserRestriction(
+                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
+    }
+
+    @Override
+    public void copy() {
+        Sliceable.setCopyContent(mContext, getSummary(),
+                mContext.getText(R.string.evolution_x_version));
     }
 }
